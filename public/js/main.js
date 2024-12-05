09ab0f0284616550f29aba436908a0ea801cd7cd
LynnFoy <lynn.f3003@gmail.com>
2024-12-05 09:59:51 +0100

    modif classe héro

--- public/js/main.js
+++ public/js/main.js
@@ -12,3 +12,38 @@ class Boss {
   }
 }
 
+// Classe de base pour les Héros
+class Hero {
+  constructor(nom, attaque, vie, posture = "attaque") {
+      this.nom = nom;
+      this.attaque = attaque;
+      this.vie = vie;
+      this.vieMax = vie;
+      this.posture = posture;
+  }
+
+  estEnVie() {
+      return this.vie > 0;
+  }
+
+  attaquer(boss) {
+      if (!this.estEnVie()) return 0;
+
+      let degats = this.attaque;
+      if (this.posture === "attaque") degats *= 1.2;
+
+      boss.vie -= degats;
+      console.log(`${this.nom} attaque ${boss.nom} et inflige ${degats.toFixed(2)} dégâts.`);
+      return degats;
+  }
+
+  subirDegats(dmg) {
+      if (!this.estEnVie()) return 0;
+
+      const dommages = this.posture === "défense" ? dmg / 2 : dmg;
+      this.vie -= dommages;
+      console.log(`${this.nom} subit ${dommages.toFixed(2)} dégâts.`);
+      return dommages;
+  }
+}
+
