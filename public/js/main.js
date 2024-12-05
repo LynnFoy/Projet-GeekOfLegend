ae7fe0b58ee9c91c7c884317542e208f7309f3d3
LynnFoy <lynn.f3003@gmail.com>
2024-12-05 09:43:48 +0100

    classe héro

--- public/js/main.js
+++ public/js/main.js
@@ -1,9 +1,26 @@
 // Classe pour les Boss
 class Boss {
-  constructor(nom, attaque, vie) {
-      this.nom = nom;
-      this.attaque = attaque;
-      this.vie = vie;
-      this.vieMax = vie;
-  }
+    constructor(nom, attaque, vie) {
+        this.nom = nom;
+        this.attaque = attaque;
+        this.vie = vie;
+        this.vieMax = vie;
+    }
+
+    estEnVie() {
+        return this.vie > 0;
+    }
 }
+
+// Classe de base pour les Héros
+class Hero {
+    constructor(nom, attaque, vie, posture = "attaque") {
+        this.nom = nom;
+        this.attaque = attaque;
+        this.vie = vie;
+        this.vieMax = vie;
+        this.posture = posture;
+    }
+  }
+
+
